81824ebe8a3a00e0077bb638c58f2b54068bb6c7
w3r1 <46977742+w3r1@users.noreply.github.com>
2019-02-26 17:34:47 +0000

    Initial checkin

---
+++ src/main/java/com/w3r1/eventLocator/EventLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.w3r1.eventLocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w3r1/eventLocator/EventLocatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.w3r1.eventLocator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventLocatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
